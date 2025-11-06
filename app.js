b4c0c28983fb199fa4565d639c2e338c3f7016d2
yoshtinup <yoshtingutierrez@gmail.com>
2025-11-05 19:55:22 -0600

    Cambio Puerto

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const createServer = require('./src/infrastructure/config/server');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Crear e iniciar el servidor
 const app = createServer();
